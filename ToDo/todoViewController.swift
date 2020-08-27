5f1f8248bff25dc8e86bb0a5f7aed2862be66ef5
AyeshaKurne <ayeshakurne823@gmail.com>
2020-08-27 13:25:08 +0530

    ToDo list complete with DataSource and Delegate Methods

---
+++ ToDo/todoViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Coditas on 25/08/20.
+//  Copyright © 2020 Coditas. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoitemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    //MARK - TableView Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        
+         
+        
+        tableView.deselectRow(at: indexPath, animated: true)  //when particular row is selected it should jst highlight once and disappear
+        tableView.cellForRow(at: indexPath)
+        
+        
+    }
+
+
+}
+
